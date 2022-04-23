4b387665d9532060d68931962b5b0188f1320111
JON ROBERT ARMSTRONG <JON.ROBERT.ARMSTRONG@GMAIL.COM>
2022-04-23 13:15:20 -0700

    QUESTION FOUR IS COMPLETE AND PASSING.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 /////////////////////////////////////
+
+let print =[];
 /* Problem 1 (this is your demo that we'll solve in class)
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
 
@@ -11,13 +13,15 @@ Test this function by hand in the console to get it working, and when you think 
 function sum(a, b) { //eslint-disable-line
     let numSum = a + b;
     let str = `The sum of ${a} and ${b} is ${numSum}.`;
-    return [numSum, str];
+    print = [numSum, str];
+    return (print) //[numSum, str];  //code after the return is unreachable by the function
 }
 sum()
-//I HAD ORIGINALLY USED sum AS THE NAME OF MY FUNCTION AND sum AS THE NAME OF THE VARIABLE IT WAS USING INTERNALLY.  WHILE THIS MADE PERFECT SENSE TO ME WITHIN THE CONFINES OF THIS ONE FUNCTION, IT MADE THINGS CONFUSING AS I TRIED TO REFERENCE THINGS ELSEWARE.  I THEN CHANGED THE FUNCTION NAME AND BROKE THE TESTER.
+//I HAD ORIGINALLY USED sum AS THE NAME OF MY FUNCTION AND sum AS THE NAME OF THE VARIABLE IT WAS USING INTERNALLY.  WHILE THIS MADE PERFECT SENSE TO ME WITHIN THE CONFINES OF THIS ONE FUNCTION, IT MADE THINGS CONFUSING AS I TRIED TO REFERENCE THINGS ELSEWHERE.  I THEN CHANGED THE FUNCTION NAME AND BROKE THE TESTER.
 
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
+console.log(print);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -33,13 +37,15 @@ Test this function by hand in the console to get it working, and when you think 
 function multiply(a, b) { //eslint-disable-line
     let product = a * b;
     let str = `The product of ${a} and ${b} is ${product}.`;
-    return [product, str];
+    print = [product, str];
+    return (print);
 
 }
 multiply()
 
 // Here is the test for multiply(); uncomment it to run it
 testMultiply(5,9);
+console.log(print);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -71,11 +77,13 @@ function sumAndMultiply(a, b, c) { //eslint-disable-line
     let product2 = multiply(product1, c)[0];
     let sumStr = a + ' and ' + b +' and ' + c +' sum to '+ sum2 +'.';
     let prodStr = 'The product of ' + a + ' and ' + b +' and ' + c +' is '+ product2 +'.';
-    return [sum2, product2, sumStr, prodStr];
+    print = [sum2, product2, sumStr, prodStr];  //returns all requests to a console.log
+    return (print);  //returns all requests as an array
 }
 sumAndMultiply()
 // Here is the test for sumAndMultiply(); uncomment it to run it
 testSumAndMultiply(4,7,5);
+console.log(print);
 
 
 //I WENT AND LOOKED AT OTHER PEOPLE'S WORK TO TRY TO FIGURE OUT WHAT I HAD DONE WRONG AND LEARNED THAT I COULD NEST ALL OF THIS.
@@ -104,9 +112,15 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) { //eslint-disable-line
-
-
+function sumArray(sumArr = []) { //eslint-disable-line  The function sumArray take in (sumArr)
+    let sum = 0;
+    for (let i = 0; i < sumArr.length; i++){
+        let currentNumber = sumArr[i];
+        sum += currentNumber;
+        }
+    print = [sum, `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sum} is their sum.`];
+    return (print);
+}
 //NOT MY CODE....  EXAMPLE FROM CLASS
 // function sumArray(sumArr) { //eslint-disable-line
 //     let (sum = 0);
@@ -116,11 +130,11 @@ function sumArray(sumArr) { //eslint-disable-line
 //         }
 //     return [sum, `${sumArr[0]}, ${sumArr[1]}, ${sumArr[0]} was passed in as an array of numbers and ${sum} is their sum.`]    
 
-}
-//sumArray()
-//Here is the test for sumArray(); uncomment it to run it
 
-//testSumArray(testArray);
+sumArray()
+//Here is the test for sumArray(); uncomment it to run it
+testSumArray(testArray);
+console.log(print);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -141,6 +155,7 @@ function multiplyArray(multArr) { //eslint-disable-line
 
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyArray(testArray);
+// console.log(print);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
@@ -169,5 +184,6 @@ function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyAnyArray(testDynamicArray);
+// console.log(print);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
